f6ad8f8f290c5463e86b7e0bb1df2f8e6f53ebda
xlp67 <thiiagowilliam@gmail.com>
2026-02-19 17:58:58 -0300

    testando workflow

--- infra/modules/gcp/build/variables.tf
+++ infra/modules/gcp/build/variables.tf
@@ -9,7 +9,7 @@ variable "region" {}
 variable "project_id" {}
 
 variable "trigger_substitutions" {
-  description = "Mapa de variáveis de substituição (substitutions) para o trigger do Cloud Build"
+  description = "Variáveis de substituição para o trigger do Cloud Build"
   type        = map(string)
   default     = {}
 }
